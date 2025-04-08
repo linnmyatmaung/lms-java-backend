1741e7e95664530d6e28ad80bdb8dc5567b949a8
linnmyatmaung <linnmyatmaung03@gmail.com>
2025-04-08 11:33:25 +0630

    initial commit

---
+++ src/main/java/com/lucus/lms_java_backend/LmsJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lucus.lms_java_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsJavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsJavaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucus/lms_java_backend/LmsJavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucus.lms_java_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsJavaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
