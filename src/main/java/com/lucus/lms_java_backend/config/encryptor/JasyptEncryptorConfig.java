bc980718acb8b9761d3398b39511478cffd3826e
linnmyatmaung <linnmyatmaung03@gmail.com>
2025-04-20 18:57:47 +0630

    add swagger-ui

---
+++ src/main/java/com/lucus/lms_java_backend/config/encryptor/JasyptEncryptorConfig.java
@@ -0,0 +1,56 @@
+/*
+ * @Author : Linn Myat Maung
+ * @Date   : 4/20/2025
+ * @Time   : 6:38 PM
+ */
+
+package com.lucus.lms_java_backend.config.encryptor;
+
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptEncryptorConfig {
+    private String password = "LMS-SYSTEM";
+
+    @Bean(name = "jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(password); // encryptor's private key
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+
+    public String encryptMessage(String plainText) {
+        var encryptor = stringEncryptor();
+        return encryptor.encrypt(plainText);
+    }
+
+    public String decryptMessage(String encryptedText) {
+        var encryptor = stringEncryptor();
+        return encryptor.decrypt(encryptedText);
+    }
+
+    public static void main(String[] args) {
+        JasyptEncryptorConfig encryptConfig = new JasyptEncryptorConfig();
+        String plainText = "admin123";
+        String encryptedString = encryptConfig.encryptMessage(plainText);
+        System.out.println("Encrypted String : " + encryptedString);
+
+        String encryptText = encryptedString;
+        String decryptString = encryptConfig.decryptMessage(encryptText);
+        System.out.println("Decrypted String : " + decryptString);
+
+    }
+}
---
+++ src/main/java/com/lucus/lms_java_backend/config/swaggerconfig/SwaggerConfig.java
@@ -0,0 +1,53 @@
+/*
+ * @Author : Linn Myat Maung
+ * @Date   : 4/20/2025
+ * @Time   : 6:54 PM
+ */
+
+package com.lucus.lms_java_backend.config.swaggerconfig;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "http://localhost:8080", description = "Local Server"),
+                @Server(url = "https://lms-java-production.up.railway.app", description = "Production Server")
+        }
+)
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addSecurityItem(
+                        new SecurityRequirement()
+                                .addList("Bearer Authentication"))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(
+                                        "Bearer Authentication",
+                                        createAPIKeyScheme()
+                                )
+                )
+                .info(new Info()
+                        .title("LMS System API")
+                        .description("LMS System")
+                        .version("1.0")
+                );
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+}
+
